b0648d15cbb3fbd0da10bd8eb3e10ef6100d3eae
courjimen <coureyfrance@gmail.com>
2025-03-24 01:52:58 -0400

    structured webpage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Sightings from './components/Sightings';
 import Form from './components/Form';
+import './App.css'
 
 function App() {
   const [sightings, setSightings] = useState([]);
