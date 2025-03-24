fed471d9a0923b2575c7af9c9f98344ac500f4b2
courjimen <coureyfrance@gmail.com>
2025-03-24 00:01:07 -0400

    added species table to db.sql

--- server/db.sql
+++ server/db.sql
@@ -1,3 +1,16 @@
+CREATE TABLE species (
+  animal_id SERIAL PRIMARY KEY, 
+  common_name TEXT NOT NULL, 
+  scientific_name TEXT UNIQUE NOT NULL, 
+  estimated_population INTEGER, 
+  conservation_status_code TEXT, 
+  record_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+
+INSERT INTO species (common_name, scientific_name, estimated_population, conservation_status_code) 
+VALUES ('Jaguar', 'Panthera onca', 173, 'NT'), 
+('Sea Turtle', 'Cheloniidae', 892, 'EN'), 
+('Bald Eagle', 'Halieetus leucocephalus', 321, 'LC');
+
 CREATE TABLE individuals (
     individual_animal_id SERIAL PRIMARY KEY,
     nickname TEXT NOT NULL,
